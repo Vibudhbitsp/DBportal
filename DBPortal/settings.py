8b7bab338284257e8fa749719878fbadb87215a1
Vibudhbitsp <80089658+Vibudhbitsp@users.noreply.github.com>
2021-07-30 01:34:28 +0530

    Update settings.py

--- DBPortal/settings.py
+++ DBPortal/settings.py
@@ -144,5 +144,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "v.shukla052002@gmail.com"
-EMAIL_HOST_PASSWORD = "vibudhbitsp"
+EMAIL_HOST_USER = "xxxxxxxxxx"
+EMAIL_HOST_PASSWORD = "xxxxxxxx"
